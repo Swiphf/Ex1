4f3499f484c34063478d5c53c6ef640923ae5696
Your Name <you@example.com>
2022-12-07 22:19:17 +0200

    script changes

--- zip_job.py
+++ zip_job.py
@@ -1,3 +1,13 @@
-testing 
-test
-testsetset
+#!/usr/bin/env python3
+
+import sys
+from os.path import exists
+
+arr = ["a", "b", "c", "d"]
+
+for item in arr:
+    try:
+        open(item + ".txt", "x")
+    except Exception as e:
+        print(f"An exception occurred : {e}")
+        sys.exit(1)
